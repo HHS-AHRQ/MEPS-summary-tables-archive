cac38276cde29b0118048f3b23f1d97cc77682aa
Mitchell <Emily.Mitchell@ahrq.hhs.gov>
2020-12-21 15:33:27 -0500

    Removing redundant code

--- build_hc_tables/code/add_caregrps.R
+++ build_hc_tables/code/add_caregrps.R
@@ -27,15 +27,7 @@ difficulty_vars <- c(
 
 if(all(difficulty_vars %in% colnames(FYC))) {
 
-  FYC <- FYC %>%
-    mutate(
-      delay_MD = (MDUNAB42 == 1|MDDLAY42==1)*1,
-      delay_DN = (DNUNAB42 == 1|DNDLAY42==1)*1,
-      delay_PM = (PMUNAB42 == 1|PMDLAY42==1)*1,
-      delay_ANY = (delay_MD|delay_DN|delay_PM)*1)
-
   ## Reason for difficulty receiving needed care
-
   FYC <- FYC %>%
     mutate(
       delay_MD  = (MDUNAB42 == 1 | MDDLAY42==1)*1,
